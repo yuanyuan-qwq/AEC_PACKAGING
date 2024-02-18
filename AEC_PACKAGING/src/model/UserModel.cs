f0e30e3879b79593a96b119d51208af5908b32c2
yuan yuan <133514345+yuanyuan-qwq@users.noreply.github.com>
2024-02-19 01:14:37 +0800

    v0.1

---
+++ AEC_PACKAGING/src/model/UserModel.cs
@@ -0,0 +1,17 @@
+﻿public class UserModel
+{
+    private string userID;
+    private string password;
+
+    public string UserID
+    {
+        get { return userID; }
+        set { userID = value; }
+    }
+
+    public string Password
+    {
+        get { return password; }
+        set { password = value; }
+    }
+}
